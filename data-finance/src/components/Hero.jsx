768dcb8a2c609f1d74212a48dcb58bea32dfe829
usanmazmert <107780733+usanmazmert@users.noreply.github.com>
2023-07-05 17:02:36 +0300

    Data Finance Typer Fixed

--- data-finance/src/components/Hero.jsx
+++ data-finance/src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import ReactTyped from 'react-typed'
+import {useTypewriter, Cursor} from "react-simple-typewriter"
 
 const Hero = () => {
+  const [text] = useTypewriter({
+    words : ["BTB", "BTC", "SASS"],
+    loop: {},
+    typeSpeed: 150,
+    delaySpeed: 3000,
+    deleteSpeed: 100
+  })
+
   return (
     <div className='px-3 text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
@@ -12,9 +20,9 @@ const Hero = () => {
           Grow with data.
         </h1>
         <p className='md:text-5xl sm:text-4xl text-xl font-bold'>
-            Fast, flexible financing for <ReactTyped strings={["BTB", "BTC", "SASS"]} typeSpeed={120} backSpeed={140} loop/>
+            Fast, flexible financing for <span className='font-bold text-gray-500'>{text}</span><span><Cursor /></span>
         </p>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <p className='md:text-2xl text-xl font-bold mt-10 text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
         <button className='bg-var-green w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
         </div>
     </div>
